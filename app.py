974172c3b272e21c38bbb0d94b01dbc980d8be24
raajakutti <72712546+raajakutti@users.noreply.github.com>
2021-03-21 23:39:04 -0500

    Updated few files

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import requests
+
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+def default():
+    return 'Welcome To TaskME!'
 
-@app.route('/admin')
-def hello_world_admin():
-    return 'Hello, AdminDocker!'
+@app.route('/dbsave')
+def dbsave():
+    response = requests.get(url="http://host.docker.internal:3000/")
+    #response = requests.post(url="http://0.0.0.0:3000/", data=paras)
+    print(response.text)
+    return str(response.text)
+
+# if __name__ == '__main__':
+#     app.run(host="taskmeui", port=8000, debug=True)
+
