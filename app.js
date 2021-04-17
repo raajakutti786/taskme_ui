44b9a3fc5a82120d9b06fc145bfe633ddcd4602d
raajakutti <72712546+raajakutti@users.noreply.github.com>
2021-04-17 14:52:58 -0500

    added few other files

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const express = require('express');
 const bodyParser = require ('body-parser')
 const localApp = require ("./helloworld")
 const path = require('path');
+const { send } = require('process');
+const request = require('request');
+const https = require('https')
+const axios = require('axios')
+const FormData = require('form-data');
+const http = require ('http')
+
 
 // Constants
 const PORT = 5000;
@@ -14,14 +21,151 @@ var urlencodedParser = bodyParser.urlencoded({extended: false})
 
 // App 
 const app = express();
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname +'/templates/index.html'));
 });
 
 app.post('/dbsave', urlencodedParser, (req, res) => {
   //console.log(req);
-  res.send(localApp.updateRequest ("Returned From DB ","Layer"));
+
+  //----------------------------------------------
+
+  // const data = JSON.stringify({
+  //   taskname: req.body.name,
+  //   taskvalue: req.body.value
+  // })
+
+  // //HTTPS Options
+  // const options = {
+  //   hostname: 'localhost',
+  //   port: 3000,
+  //   path: '/dbsave',
+  //   method: 'POST',
+  //   rejectUnauthorized: false,
+  //   requestCert: false,
+  //   agent: false,
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //     'Content-Length': data.length
+  //   }
+  // }
+
+  // const req1 = https.request(options, res1 => {
+  //   console.log(`statusCode: ${res.statusCode}`)
+  
+  //   res1.on('data', d => {
+  //     process.stdout.write(d)
+  //   })
+  // })
+  
+  // req1.on('error', error => {
+  //   console.error(error)
+  // })
+  
+  // req1.write(data)
+  // req1.end()
+  //----------------------------------------------------
+
+
+//--------------------------------------------------------
+const data = JSON.stringify({
+  taskname: req.body.name,
+  taskvalue: req.body.value
+})
+
+//HTTP Options
+var options = {
+  host: 'localhost',
+  port: 3000,
+  path: '/dbsave',
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      //'Content-Type': 'application/json',
+      'Content-Length': Buffer.byteLength(data)
+  }
+};
+
+var req10 = http.request(options, function(res10) {
+    res10.setEncoding('utf8');
+    res10.on('data', function (chunk) {
+        console.log("body: " + chunk);
+    });
 });
+
+req10.write(data);
+req10.end();
+//---------------------------------------------------------
+
+
+
+  //------------------------------------------------------
+  // const formData = new FormData();
+
+  // formData.append ("taskname", req.body.name)
+  // formData.append ("taskvalue", req.body.value)
+
+  // let axiosConfig = {
+  //   headers: {
+  //       'Content-Type': 'application/json;charset=UTF-8',
+  //       "Access-Control-Allow-Origin": "*",
+  //   }
+  // };
+
+  // const body = { 'username' : 'password' };
+
+  // axios.post('http://localhost:3000/dbsave', body, axiosConfig)
+  // .then(res55 => console.log(res))
+  // .catch(err => console.log('Login: ', err));
+  
+
+  // axios
+  // .post('http://localhost:3000/dbsave', formData, {
+  //   //Test
+  // })
+  // .then(res1 => {
+  //   console.log(`statusCode: ${res1.statusCode}`)
+  //   console.log(res1)
+  // })
+  // .catch(error => {
+  //   console.error(error)
+  // })
+//----------------------------------------------------------
+
+
+  res.send(localApp.updateRequest ("Saved To DB ","Layer"));
+});
+
+
+
+app.post('/load', urlencodedParser, (req, res) => {
+
+  //-------------------------------------------------------------------------------
+  // request('http://localhost:3000/load', { json: true }, (err, res, body) => {
+  //   if (err) { return console.log(err); }
+  //   console.log(body.url);
+  //   console.log(body.explanation);
+  // })
+  //-------------------------------------------------------------------------------
+
+  //-------------------------------------------------------------------------------
+  axios
+  .get('http://localhost:3000/load', {
+    todo: 'Buy the milk'
+  })
+  .then(res1 => {
+    console.log(`statusCode: ${res1.statusCode}`)
+    console.log(res1)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+  res.sendFile(path.join(__dirname +'/templates/index.html'));
+  //res.send(localApp.updateRequest ("Load:","Layer"));
+});
+//-----------------------------------------------------------------------------------
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
--- test/test_server.js
+++ test/test_server.js
@@ -10,7 +10,7 @@ describe('GET /', function () {
    //This is testing local code
     it ('Updating Incoming requests', () => {
       const result = localApp.updateRequest ("Returned From DB ","Layer")
-      chai.assert.equal (result, "Returned From DB")
+      chai.assert.equal (result, "Returned From DB Layer")
     })
 });
 
